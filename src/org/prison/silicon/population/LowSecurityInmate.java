f82d1c2c011409c3fdc8682865905a0b0e401760
Robert Martinez <robertez@amazon.com>
2023-09-15 14:36:30 -0500

    Initial org.prision.silicon package commit with the following classes [Inmate, LowSecurityInmate, MediumSecurityInmate, HighSecurityInmate] also deleted the default DeleteMe files

---
+++ src/org/prison/silicon/population/LowSecurityInmate.java
@@ -0,0 +1,30 @@
+package org.prison.silicon.population;
+
+import java.time.LocalDate;
+
+class LowSecurityInmate extends Inmate{
+
+    public LowSecurityInmate(int idNumber, String name, LocalDate sentenceLength, boolean gangLeader, SecurityRating securityRating){
+        super(idNumber, name, sentenceLength, gangLeader, securityRating);
+    }
+
+    @Override
+    public void work() {
+        // move to workshop
+    }
+
+    @Override
+    public void move(FacilityList location) {
+        // move from current location to the passed ENUM location
+    }
+
+    @Override
+    public void eat() {
+        // move inmates from their current location to the kitchen
+    }
+
+    @Override
+    public void sleep() {
+        // move inmates from their current location to the unit for sleep
+    }
+}
