48706081d0593ee87b754a70827f2e3de326f592
Chris Covey <chris.covey@tlgcohort.com>
2023-09-19 06:19:38 -0400

    Adding logic to load inmate-data into client then add each inmate into their respective SecurityUnit

--- src/org/prison/silicon/client/client.java
+++ src/org/prison/silicon/client/client.java
@@ -5,9 +5,13 @@ import org.prison.silicon.FacilityList;
 import org.prison.silicon.Prison;
 import org.prison.silicon.SecurityRating;
 import org.prison.silicon.gui.MainGui;
+import org.prison.silicon.population.Inmate;
+import org.prison.silicon.population.InmateLoader;
 
 import java.io.IOException;
+import java.util.List;
 
+// TODO: Look at adding controller to a .app package to control game like in DuckRace
 public class client {
     public static Facility lowSecurityUnit = new Facility(FacilityList.LOW_SECURITY_UNIT, 15, SecurityRating.LOW);
     public static Facility mediumSecurityUnit = new Facility(FacilityList.MEDIUM_SECURITY_UNIT, 15, SecurityRating.MEDIUM);
@@ -19,6 +23,22 @@ public class client {
 
 
     public static void main(String[] args) throws IOException {
+        // Adding inmateLoadList to appropriate SecurityUnit based upon securityRating
+        InmateLoader inLoader = new InmateLoader("resources/data/inmate-data.csv");
+        List<Inmate> inmateLoadList = inLoader.load();
+        for (int i = 0; i < inmateLoadList.size(); i++) {
+            switch (inmateLoadList.get(i).getSecurityRating()) {
+                case LOW:
+                    lowSecurityUnit.addInmate(inmateLoadList.get(i));
+                    break;
+                case MEDIUM:
+                    mediumSecurityUnit.addInmate(inmateLoadList.get(i));
+                    break;
+                case HIGH:
+                    highSecurityUnit.addInmate(inmateLoadList.get(i));
+                    break;
+            }
+        }
 
         // launch GUI
         MainGui mainGui = new MainGui();
